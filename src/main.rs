741c90822ddd17f038f5dd7844bd6858a1f468f9
dinislam tebuev <3241533+tebuevd@users.noreply.github.com>
2023-10-22 14:05:50 -0700

    reads an eml file and prints the content to std out

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
+use mail_parser::*;
 use std::env;
-use std::fs;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
 
 fn main() {
     let filepath = env::args().nth(1).expect("no file given");
+    let f = File::open(filepath).expect("can't read the file");
 
-    let contents = fs::read_to_string(filepath).expect("something went wrong reading the file");
+    let mut reader = BufReader::new(f);
+    let mut vec = Vec::new();
+    let _ = reader.read_to_end(&mut vec);
 
-    println!("{}", contents);
+    let message = MessageParser::default().parse(&vec).unwrap();
+
+    let to: Vec<_> = message
+        .to()
+        .unwrap()
+        .iter()
+        .map(|x| x.address().unwrap())
+        .collect();
+    let from: Vec<_> = message
+        .from()
+        .unwrap()
+        .iter()
+        .map(|x| x.address().unwrap())
+        .collect();
+    let subject = message.subject().unwrap();
+    let attachment_count = message.attachment_count();
+    let body = message.body_text(0).unwrap();
+
+    println!("From: {:?}", from);
+    println!("To: {:?}", to);
+    println!("Subject: {:?}", subject);
+    println!("Body: {:?}", body);
+    println!("Attachment count: {:?}", attachment_count);
 }

f3d802b2945985605a432fea18a26afe801a919b
dinislam tebuev <3241533+tebuevd@users.noreply.github.com>
2023-11-18 16:36:18 -0800

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use json;
 use json::object;
 use json::JsonValue;
+use mail_parser::Address;
 use mail_parser::HeaderValue;
 use mail_parser::*;
 use std::env;
@@ -8,15 +9,26 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::Read;
 
-fn main() {
-    let filepath = env::args().nth(1).expect("no file given");
-    let f = File::open(filepath).expect("can't read the file");
+fn addresses_to_json(addrs: Option<&Address>) -> JsonValue {
+    return match addrs {
+        None => JsonValue::new_array(),
+        Some(addrs) => {
+            return addrs
+                .iter()
+                .map(|item| {
+                    let mut obj = JsonValue::new_object();
+                    obj["name"] = item.name().into();
+                    obj["address"] = item.address().into();
+                    return obj;
+                })
+                .collect::<Vec<_>>()
+                .into()
+        }
+    };
+}
 
-    let mut reader = BufReader::new(f);
-    let mut vec = Vec::new();
-    let _ = reader.read_to_end(&mut vec);
-
-    let message = MessageParser::default().parse(&vec).unwrap();
+pub fn eml_to_json(data: &[u8]) -> String {
+    let message = MessageParser::default().parse(data).unwrap();
 
     let attachment_count = message.attachment_count();
     let body = message.body_text(0).unwrap();
@@ -54,36 +66,8 @@ fn main() {
 
     let mut json_obj = JsonValue::new_object();
 
-    json_obj["from"] = match message.from() {
-        None => JsonValue::new_array(),
-        Some(item) => item
-            .iter()
-            .map(|i| {
-                let mut data = JsonValue::new_object();
-
-                data["name"] = i.name().into();
-                data["address"] = i.address().into();
-                return data;
-            })
-            .collect::<Vec<_>>()
-            .into(),
-    };
-
-    json_obj["to"] = match message.to() {
-        None => JsonValue::new_array(),
-        Some(to_fields) => to_fields
-            .iter()
-            .map(|address| {
-                let mut data = JsonValue::new_object();
-
-                data["name"] = address.name().into();
-                data["address"] = address.address().into();
-
-                return data;
-            })
-            .collect::<Vec<_>>()
-            .into(),
-    };
+    json_obj["from"] = addresses_to_json(message.from());
+    json_obj["to"] = addresses_to_json(message.to());
 
     json_obj["subject"] = match message.subject() {
         Some(subject) => subject.into(),
@@ -100,5 +84,16 @@ fn main() {
     json_obj["body_html"] = body_html.to_string().into();
     json_obj["raw_headers"] = headers_arr;
 
-    println!("{:#}", json_obj);
+    return json_obj.to_string();
+}
+
+fn main() {
+    let filepath = env::args().nth(1).expect("no file given");
+    let f = File::open(filepath).expect("can't read the file");
+
+    let mut reader = BufReader::new(f);
+    let mut vec = Vec::new();
+    let _ = reader.read_to_end(&mut vec);
+
+    println!("{:#}", eml_to_json(&vec));
 }
